43579b232ec325d721fda5cebdf5bf3ef5f054f0
StanislavGS <s_stanislavov@abv.bg>
2016-11-10 20:55:33 +0200

    Maybe ready

--- 7_OOPPrinciples/Task2_UniversityManagement/src/Task2_UniversityManagement.java
+++ 7_OOPPrinciples/Task2_UniversityManagement/src/Task2_UniversityManagement.java
@@ -13,21 +13,48 @@ import java.util.Scanner;
  */
 public class Task2_UniversityManagement {
 
-    public static void changeTolerance(Object[] persons,double balance, int... change) {
+    public static void changeTolerance(Object[] persons, double balance, int... change) {
         for (Object person : persons) {
             if (person instanceof MaintenanceEmployee) {
                 ((MaintenanceEmployee) person).increaseTolerance(change[0]);
+                checkForNegativeTolerance(person);
             }
             if (person instanceof AdministrationEmployee) {
                 ((AdministrationEmployee) person).increaseTolerance(change[1]);
+                checkForNegativeTolerance(person);
             }
             if (person instanceof Teacher) {
                 ((Teacher) person).increaseTolerance(change[2]);
+                checkForNegativeTolerance(person);
             }
             if (person instanceof Student) {
                 ((Student) person).increaseTolerance(change[3]);
+                checkForNegativeTolerance(person);
             }
         }
+    }
+
+    static void checkForNegativeTolerance(Object obj) {
+        if (((Person) obj).tolerance <= 0) {
+            System.out.println(((Person) obj).getName() + " is not happy.");
+            System.exit(0);
+        }
+    }
+    
+    static int findByName(String name,Object[] persons,int numPersons){
+        int idx=0;
+        while(idx<numPersons ){ 
+            String st=((Person) persons[idx]).getName();
+            if(st.equals(name)){
+                break;
+            }
+            idx++;
+        }
+        if(idx<numPersons){
+            return idx;
+        }else{
+            return -1;
+        }            
     }
 
     public static void main(String[] args) {
@@ -43,13 +70,36 @@ public class Task2_UniversityManagement {
                 String[] elements = st.split(" ");
                 switch (elements[0]) {
                     case "WORK":
+                        int idxPerson = findByName(elements[1], persons, numPersons);
+                        if (idxPerson >= 0) {
+                            if (persons[idxPerson] instanceof MaintenanceEmployee) {
+                                changeTolerance(persons, balance, new int[]{2, 2, 2, 2});
+                                balance -= 15;
+                            }
+                            if (persons[idxPerson] instanceof AdministrationEmployee) {
+                                changeTolerance(persons, balance, new int[]{1, 0, 3, 3});
+                                balance -= 19;
+                            }
+                            if (persons[idxPerson] instanceof Teacher) {
+                                changeTolerance(persons, balance, new int[]{-3, -1, 0, 3});
+                                balance -= 25;
+                            }
+                            if (persons[idxPerson] instanceof Student) {
+                                changeTolerance(persons, balance, new int[]{-1, 0, 0, 2});
+                                if (((Student) persons[idxPerson]).tolerance >= 50) {
+                                    balance += 50;
+                                }
+                            }
+                            if (balance <= 0) {
+                                System.out.println("Bankrupcy.");
+                                System.exit(0);
+                            }
+                        }
+
                         break;
                     case "IDLE":
                         for (Object person : persons) {
-                            if (person instanceof MaintenanceEmployee) {
-                                balance -= 15;
-
-                            }
+                            changeTolerance(persons, balance, new int[]{-5, -5, -5, -5});
                         }
                         break;
                     case "NEW":
