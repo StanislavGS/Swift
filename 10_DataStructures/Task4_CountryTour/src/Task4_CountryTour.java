fcdfc576f44c053bd99b34901ca588f07ef9d598
StanislavGS <s_stanislavov@abv.bg>
2016-11-22 20:41:44 +0200

    ready

--- 10_DataStructures/Task4_CountryTour/src/Task4_CountryTour.java
+++ 10_DataStructures/Task4_CountryTour/src/Task4_CountryTour.java
@@ -1,4 +1,8 @@
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /*
@@ -6,45 +10,67 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Stanislav Stanislavov
  */
 public class Task4_CountryTour {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
-        int n=Integer.parseInt(sc.nextLine());
-        int[] distanceToNextCity=new int[n];
-        int[] neededFood=new int[n]; 
-        int min=Integer.MAX_VALUE,idxMin=-1;
-        int sum=0;
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        int[] distAndFood = new int[2];
+        List<DistAndFood> distancesAndFood = new ArrayList<>();
+        int min = Integer.MAX_VALUE, idxMin = -1;
+        int sum = 0;
+
         for (int i = 0; i < n; i++) {
-            String[] st=sc.nextLine().split(" ");
-            distanceToNextCity[i]=Integer.parseInt(st[0]);
-            neededFood[i]=Integer.parseInt(st[1]);
-            sum+=distanceToNextCity[i]-neededFood[i];
-            if (min>sum){
-                min=sum;
-                idxMin=i;
-            }            
-        }
-        
-        sum=0;
-        int idx=idxMin+1;
-        if (idx==n){
-            idx=0;
-        }
-        boolean result=true;
-        for (int i = 0; i < n; i++) {
-            sum+=distanceToNextCity[idx]-neededFood[idx];
-            if (sum<0) {
-                result=false;
-                break;
+            String[] st = sc.nextLine().split(" ");
+            //in original condition is reversed
+            //distancesAndFood.add(i, new DistAndFood(Integer.parseInt(st[1]), Integer.parseInt(st[0])));
+            distancesAndFood.add(i, new DistAndFood(Integer.parseInt(st[1]), Integer.parseInt(st[0])));
+            sum += distancesAndFood.get(i).getDiastance() - distancesAndFood.get(i).getFood();
+            if (min >= sum) {//min>sum - is bettter(but the answer for first example is 4 --not like in example)
+                min = sum;
+                idxMin = i;
             }
         }
-        
-        System.out.println(result?idxMin:"NO");
+
+        sum = 0;
+        idxMin++;
+        if (idxMin == n) {
+            idxMin = 0;
+        }
+        boolean result = true;
+        for (int i = idxMin; i < idxMin + n; i++) {
+            sum += distancesAndFood.get(i % n).getDiastance() - distancesAndFood.get(i % n).getFood();
+            if (sum < 0) {
+                result = false;
+                break;
+            }
+
+        }
+
+        System.out.println(result ? idxMin + 1 : "NO");//Show the number of city. Numbers started from 1.
+    }
+}
+
+class DistAndFood {
+
+    private int distance;
+    private int food;
+
+    public int getDiastance() {
+        return this.distance;
+    }
+
+    public int getFood() {
+        return this.food;
+    }
+
+    DistAndFood(int distance, int food) {
+        this.distance = distance;
+        this.food = food;
     }
 }
