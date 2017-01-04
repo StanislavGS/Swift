48b56648c9297ad0ad9b4d04e5c7672fc5d7c0da
stanislav <stanislav@stanislav-compaq-nx9020-PG618EA-ABB>
2017-01-04 08:50:33 +0200

    Not ready

--- 14_JDBC/Task1_School/src/dataBaseTransfer/TaskFunctions.java
+++ 14_JDBC/Task1_School/src/dataBaseTransfer/TaskFunctions.java
@@ -24,7 +24,7 @@ public class TaskFunctions {
     final static String DBMS_USERNAME = "root";
     final static String DBMS_PASSWORD = "";
 
-    public static int insertTeacher(Teacher teacher) {
+    public static int insertTeacher(Teacher teacher) throws SQLException {
         int idAddress = insertQuery("addresses", new String[]{"country", teacher.getAddress().getCountry(),
             "city", teacher.getAddress().getCity(),
             "street", teacher.getAddress().getStreet(),
@@ -54,9 +54,9 @@ public class TaskFunctions {
                         rs.getInt("floor"), rs.getInt("apartmentNo")));
             }
         } finally {
-            con.close();
-            statement.close();
-            rs.close();
+            if (con!=null) con.close();
+            if (statement!=null) statement.close();
+            if (rs!=null) rs.close();
         };
         return teacher;
     }
@@ -87,8 +87,8 @@ public class TaskFunctions {
 
     }
 
-    public static int insertStudent(String name, LocalDate enrollmentDate, Address address) {
-int idAddress = insertQuery("addresses", new String[]{"country", address.getCountry(),
+    public static int insertStudent(String name, LocalDate enrollmentDate, Address address) throws SQLException {
+        int idAddress = insertQuery("addresses", new String[]{"country", address.getCountry(),
             "city", address.getCity(),
             "street", address.getStreet(),
             "number", address.getNumber(),
@@ -206,7 +206,42 @@ int idAddress = insertQuery("addresses", new String[]{"country", address.getCoun
         rs = statement.executeQuery(query);
     }
 
-    private static int insertQuery(String table, String[] colsAndValues) {
-
+    private static int insertQuery(String table, String[] colsAndValues) throws SQLException {
+        String select = "SELECT id FROM `" + table + "` WHERE ";
+        String insertPart1 = "INSERT INTO `" + table + "` (";
+        String insertPart2 = " VALUES (";
+        for (int i = 0; i < colsAndValues.length / 2; i++) {
+            select += "`" + colsAndValues[i * 2] + "`='" + colsAndValues[i * 2 + 1] + "'";
+            insertPart1 += "`" + colsAndValues[i * 2] + "`";
+            insertPart2 += "'" + colsAndValues[i * 2 + 1] + "'";
+            if (i <colsAndValues.length / 2 -1) {
+                select += " AND ";
+                insertPart1 += ",";
+                insertPart2 += ",";
+            } else {
+                select += ";";
+                insertPart1 += ")";
+                insertPart2 += ")";
+            }
+        }
+        ResultSet rs = null;
+        try (Connection con = DriverManager.getConnection(DBMS_CONN_STRING, DBMS_USERNAME, DBMS_PASSWORD);
+                Statement statement = con.createStatement();) {
+            rs = statement.executeQuery(select);
+            if (rs.next()) {
+                return rs.getInt(1);
+            } else {
+                statement.execute(insertPart1 + insertPart2,Statement.RETURN_GENERATED_KEYS);
+                rs= statement.getGeneratedKeys();
+                if(rs.next()){
+                    return rs.getInt(1);
+                }
+            }
+        } finally {
+            if(rs!=null){
+                rs.close();
+            }
+        }
+        return -1;
     }
 }
--- 14_JDBC/Task1_School/src/task1_school/Task1_School.java
+++ 14_JDBC/Task1_School/src/task1_school/Task1_School.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package task1_school;
+
 import Objects.*;
 import dataBaseTransfer.*;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -14,15 +16,35 @@ import java.util.ArrayList;
  * @author stanislav
  */
 public class Task1_School {
-public static void main(String[] args) {
-        TaskFunctions.insertTeacher(new Teacher("Peatar Toshev","petar.toshev@gmail.com",1450,
-                new Address("Bulgaria","Sofia","rd. Gotse Deltchev","125-B",3,15)));
-        
-        Teacher teacher= TaskFunctions.getTeacher(2);
-        ArrayList<Teacher> teachers=TaskFunctions.teachersWithSpSalary(1000,2000);
-        TaskFunctions.insertStudent("Ivancho Mladenov",new  LocalDate(1.1.2017), 
-                new Address("Bulgaria","Sofia","Pirotska","175",2,3));
-        
+
+    public static void main(String[] args) {
+
+        try {
+            int id1=TaskFunctions.insertTeacher(new Teacher("Peatar Toshev", "petar.toshev@gmail.com", 1450,
+                    new Address("Bulgaria", "Sofia", "rd. Gotse Deltchev", "125-B", 3, 19)));
+
+            Teacher teacher = TaskFunctions.getTeacher(2);
+            
+            ArrayList<Teacher> teachers = TaskFunctions.teachersWithSpSalary(1000, 2000);
+
+            int id2=TaskFunctions.insertStudent("Ivancho Mladenov", LocalDate.of(2016, 9, 15),
+                    new Address("Bulgaria", "Sofia", "Pirotska", "175", 2, 3));
+            
+            Student student=TaskFunctions.getStudent(3);
+            
+            ArrayList<Student> students=TaskFunctions.getStudents(LocalDate.of(2010 ,1,1));
+            
+            ArrayList<String> disciplines=TaskFunctions.getDisciplinesByTeacherId(1);
+            
+            ArrayList<Teacher> teachers1=TaskFunctions.getTeachersByDisciplineName("Mathematics");
+
+        } catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println(ex.getSQLState());
+                System.out.println(ex.getMessage());
+                System.out.println(ex.getErrorCode());
+                ex = ex.getNextException();
+            }
+        }
     }
-    
 }
